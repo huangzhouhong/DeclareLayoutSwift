86a6eb05e4c9643cd202f52fa81114dcd04798b4
huangzhouhong <778612084@qq.com>
2018-03-30 23:53:30 +0800

    Change Navigation Bar TitleView

--- Dingding/Dingding/UIViewController+TitleView.swift
+++ Dingding/Dingding/UIViewController+TitleView.swift
@@ -6,20 +6,20 @@
 //  Copyright © 2018年 com.yasoon. All rights reserved.
 //
 
-import  UIKit
 import DeclareLayoutSwift
+import UIKit
 
-extension UIViewController{
-    func createTitleView(title:String){
+extension UIViewController {
+    func createTitleView(title: String) {
         self.navigationController?.navigationBar.barTintColor = .white
         let titleView = HostView {
-            Grid {
+            Grid(.columns <- [.star(1), .auto]) {
                 [Label(.text <- title, .fontSize <- 20),
-                 StackPanel(.orientation <- .Horizontal, .hAlign <- .Right) {
-                    [Button(.image <- #imageLiteral(resourceName: "Msg_nav1")),
-                     Button(.image <- #imageLiteral(resourceName: "Msg_nav2"), .margin <- Insets(vertical: 0, horizontal: 20)),
-                     Button(.image <- #imageLiteral(resourceName: "Msg_nav3"))]
-                    }]
+                 StackPanel(.gridColumnIndex <- 1, .orientation <- .Horizontal) {
+                     [Button(.image <- #imageLiteral(resourceName: "Msg_nav1")),
+                      Button(.image <- #imageLiteral(resourceName: "Msg_nav2"), .margin <- Insets(vertical: 0, horizontal: 20)),
+                      Button(.image <- #imageLiteral(resourceName: "Msg_nav3"))]
+                }]
             }
         }
         self.navigationItem.titleView = titleView
